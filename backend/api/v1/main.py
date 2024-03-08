b0ad780662fc88222343e65c63799bb9c8e5bda2
Andrey <ashilkov2000@mail.ru>
2024-03-08 20:29:45 +0500

    added auth prefix to auth endpoints

--- backend/api/v1/main.py
+++ backend/api/v1/main.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from backend.api.v1.routes import user, auth
 
 api_router = APIRouter()
-api_router.include_router(auth.router, tags=["login"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
