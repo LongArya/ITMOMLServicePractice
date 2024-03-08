1201dcd303ceef92dba0613580572415c0e21269
Andrey <ashilkov2000@mail.ru>
2024-03-08 16:18:51 +0500

    made user schemas name more verbose

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -3,33 +3,24 @@ from typing import Optional
 
 
 # Shared properties
-class UserBase(BaseModel):
-    full_name: Optional[str] = None
-    email: Optional[EmailStr] = None
+class UserBaseSchema(BaseModel):
+    name: str
+    email: EmailStr
     is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserReadSchema(BaseModel):
+    pass
 
 
 # Properties to receive via API on creation
-class UserCreate(UserBase):
-    email: EmailStr
+class UserCreateSchema(UserBaseSchema):
     password: str
 
 
 # Properties to receive via API on update
-class UserUpdate(UserBase):
+class UserUpdateSchema(UserBaseSchema):
     password: str | None = None
-
-
-class UserInDBBase(UserBase):
-    id: int | None = None
-    model_config = ConfigDict(from_attributes=True)
-
-
-# Additional properties to return via API
-class User(UserInDBBase):
-    pass
-
-
-# Additional properties stored in DB
-class UserInDB(UserInDBBase):
-    hashed_password: str
