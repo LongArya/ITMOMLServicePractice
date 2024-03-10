b7ee822369c414da5ce1caee116c168dd0588c4d
Andrey <ashilkov2000@mail.ru>
2024-03-10 22:42:17 +0500

    added models db initialization

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,10 +1,28 @@
 from backend.models import User, Prediction, Model
 from backend.core.config import settings
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session, select
+from backend.crud import get_model_by_name
 
 
 engine = create_engine(str(settings.SQLALCHEMY_SQLITE_DATABASE_URI))
 
 
+def init_models():
+    log_reg_model = Model(name="LogReg", cost=100)
+    dtree = Model(name="Dtree", cost=200)
+    random_forest = Model(name="RandomForest", cost=300)
+
+    with Session(engine) as session:
+        for model in [log_reg_model, dtree, random_forest]:
+            existing_model = get_model_by_name(session=session, model_name=model.name)
+            print(f"existing_model = {existing_model}")
+            if existing_model is None:
+                session.add(model)
+                session.commit()
+            else:
+                print("skip adding models")
+
+
 def init_db():
     SQLModel.metadata.create_all(engine)
+    init_models()
--- backend/crud.py
+++ backend/crud.py
@@ -4,7 +4,7 @@ from sqlmodel import Session, select
 
 from backend.core.security import get_password_hash, verify_password
 from backend.schemas.user import UserCreateSchema
-from backend.models import User
+from backend.models import User, Model
 from backend.core.config import settings
 
 
@@ -26,6 +26,12 @@ def get_user_by_email(session: Session, email: str) -> Optional[User]:
     statement = select(User).where(User.email == email)
     session_user = session.exec(statement).first()
     return session_user
+
+
+def get_model_by_name(session: Session, model_name: str) -> Optional[Model]:
+    statement = select(Model).where(Model.name == model_name)
+    session_model = session.exec(statement).first()
+    return session_model
 
 
 def authenticate(session: Session, email: str, password: str) -> Optional[User]:
