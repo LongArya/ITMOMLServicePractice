bfe5097057ecb45105d0f6cfd907d50ef444a394
Andrey <ashilkov2000@mail.ru>
2024-03-11 02:31:42 +0500

    added endpoints for prediction via redis

--- backend/api/v1/routes/prediction.py
+++ backend/api/v1/routes/prediction.py
@@ -1,30 +1,94 @@
-from typing import Any
-
-from fastapi import APIRouter
+from typing import Any, Optional
+from http import HTTPStatus
+from fastapi import APIRouter, HTTPException
 from sqlmodel import delete, func, select
-from backend.api.v1.dependencies import (
-    SessionDep,
-)
+from backend.api.v1.dependencies import SessionDep, CurrentUserDep
 from pydantic import BaseModel, ConfigDict
 from enum import Enum, auto
+from rq import Queue
+from rq.job import Job as RqJob
+from redis import Redis
+from worker import worker_function
+from backend.models import Model, Prediction
+from backend.schemas.prediction import (
+    PredictionCreateSchema,
+    PreductionCreationOutput,
+    PredictionOutSchema,
+)
+from backend.crud import (
+    get_model_by_name,
+    create_prediction_item,
+    make_payment_for_user,
+    update_prediction_after_fail,
+    update_prediction_after_successfull_finish,
+)
 
-
-class GeneState(str, Enum):
-    MUTATED = "MUTATED"
-    NOT_MUTATED = "NOT_MUTATED"
-
-
-class GliomaClassifierInput(BaseModel):
-    IDH1: GeneState
-
+redis_conn = Redis()
+queue = Queue(connection=redis_conn)
+RQ_JOB_FINISHED_STATUS = "finished"
+RQ_JOB_FAILED_STATUS = "failed"
 
 router = APIRouter()
 
 
-@router.post("/")
-def test_pred(session: SessionDep, input: GliomaClassifierInput) -> Any:
+@router.get("/{prediction_id}", response_model=PredictionOutSchema)
+def get_prediction_result(
+    prediction_id: int,
+    session: SessionDep,
+    current_user: CurrentUserDep,
+):
+    print("GET RESULT")
+    prediction: Optional[Prediction] = session.get(Prediction, prediction_id)
+    print(f"prediction")
+    print(prediction)
+    if prediction is None:
+        raise HTTPException(status_code=404, detail="Invalid prediction id")
+    if prediction.user_id != current_user.id:
+        raise HTTPException(
+            HTTPStatus.FORBIDDEN,
+            detail="Users are allowed to see only their predictions",
+        )
+    if prediction.rq_status in [RQ_JOB_FINISHED_STATUS, RQ_JOB_FAILED_STATUS]:
+        return prediction
+    print("job id = ", prediction.rq_job_id)
+    rq_job: RqJob = RqJob.fetch(prediction.rq_job_id, connection=redis_conn)
+    if rq_job.get_status() == RQ_JOB_FINISHED_STATUS:
+        prediction = update_prediction_after_successfull_finish(
+            session=session, prediction_id=prediction_id, result=rq_job.result
+        )
+    if rq_job.get_status() == RQ_JOB_FAILED_STATUS:
+        prediction = update_prediction_after_fail(rq_job.result)
+    return prediction
+
+
+@router.post("/{model_name}", response_model=PreductionCreationOutput)
+def start_prediction(
+    model_name: str,
+    session: SessionDep,
+    current_user: CurrentUserDep,
+    prediction_create_input: PredictionCreateSchema,
+) -> Any:
     """
     Retrieve users.
     """
-    a = input
-    return input
+    model: Optional[Model] = get_model_by_name(session=session, model_name=model_name)
+    if model is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Such model does not exist"
+        )
+    rq_job = queue.enqueue(worker_function, model_name, prediction_create_input)
+    if model.cost > current_user.balance:
+        raise HTTPException(
+            status_code=HTTPStatus.PAYMENT_REQUIRED,
+            detail="Not enough credits for this operation",
+        )
+    make_payment_for_user(session=session, user_id=current_user.id, payment=model.cost)
+    created_prediction = create_prediction_item(
+        session=session,
+        model_id=model.id,
+        user_id=current_user.id,
+        prediction_input=prediction_create_input,
+        rq_job_id=rq_job.id,
+        rq_status=rq_job.get_status(),
+    )
+    return created_prediction
