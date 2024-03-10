2833eb107a7e8db3e8a9f8a38c9bf59d56bc5601
Andrey <ashilkov2000@mail.ru>
2024-03-11 02:31:54 +0500

    added operations for predictions and users

--- backend/crud.py
+++ backend/crud.py
@@ -1,11 +1,14 @@
 from typing import Any, Optional
-
+import datetime
 from sqlmodel import Session, select
-
 from backend.core.security import get_password_hash, verify_password
 from backend.schemas.user import UserCreateSchema
-from backend.models import User, Model
+from backend.schemas.prediction import PredictionCreateSchema
+from backend.models import User, Model, Prediction
 from backend.core.config import settings
+
+RQ_JOB_FINISHED_STATUS = "finished"
+RQ_JOB_FAILED_STATUS = "failed"
 
 
 def create_user(session: Session, user_create: UserCreateSchema) -> User:
@@ -20,6 +23,83 @@ def create_user(session: Session, user_create: UserCreateSchema) -> User:
     session.commit()
     session.refresh(db_obj)
     return db_obj
+
+
+def make_payment_for_user(session: Session, user_id: int, payment: int) -> None:
+    db_obj = session.get(User, user_id)
+    reduced_balance = db_obj.balance - payment
+    if reduced_balance < 0:
+        raise ValueError("Tried to commit payment without enough credits")
+    db_obj.balance = reduced_balance
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+
+
+def return_payment_to_user(session: Session, user_id: int, payment: int) -> None:
+    db_obj = session.get(User, user_id)
+    updated_balance = db_obj.balance + payment
+    db_obj.balance = updated_balance
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+
+
+def create_prediction_item(
+    session: Session,
+    model_id: int,
+    user_id: int,
+    rq_job_id: str,
+    rq_status: str,
+    prediction_input: PredictionCreateSchema,
+) -> Prediction:
+    inferred_model: Model = session.get(Model, model_id)
+    current_timestamp: datetime.datetime = datetime.datetime.now()
+    db_obj = Prediction.model_validate(
+        prediction_input,
+        update={
+            "model_id": model_id,
+            "user_id": user_id,
+            "started_at": current_timestamp,
+            "cost": inferred_model.cost,
+            "rq_job_id": rq_job_id,
+            "rq_status": rq_status,
+        },
+    )
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+    return db_obj
+
+
+def update_prediction_after_successfull_finish(
+    session: Session, prediction_id: int, result: str
+) -> Prediction:
+    db_prediction: Prediction = session.get(Prediction, prediction_id)
+    if db_prediction.rq_status == RQ_JOB_FINISHED_STATUS:
+        return db_prediction
+
+    db_prediction.rq_status = RQ_JOB_FINISHED_STATUS
+    db_prediction.result = result
+    session.add(db_prediction)
+    session.commit()
+    session.refresh(db_prediction)
+    return db_prediction
+
+
+def update_prediction_after_fail(session: Session, prediction_id: int) -> None:
+    db_prediction: Prediction = session.get(Prediction, prediction_id)
+    if db_prediction.rq_status == RQ_JOB_FINISHED_STATUS:
+        return db_prediction
+
+    db_prediction.rq_status = RQ_JOB_FAILED_STATUS
+    session.add(db_prediction)
+    session.commit()
+    session.refresh(db_prediction)
+    return_payment_to_user(
+        session=session, user_id=db_prediction.user_id, payment=db_prediction.cost
+    )
+    return db_prediction
 
 
 def get_user_by_email(session: Session, email: str) -> Optional[User]:
