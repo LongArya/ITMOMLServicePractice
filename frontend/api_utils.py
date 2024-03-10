a359f6aa7f94f52c34ab0f4504172e731af53ff0
Andrey <ashilkov2000@mail.ru>
2024-03-10 22:13:43 +0500

    added current user info to api utils

--- frontend/api_utils.py
+++ frontend/api_utils.py
@@ -1,4 +1,5 @@
 import requests
+from http import HTTPStatus
 from pprint import pprint
 from requests import Response
 from backend.core.config import settings
@@ -6,7 +7,7 @@ from backend.core.config import settings
 BACKEND_API = "http://127.0.0.1:8000"
 REGISTER_ENDPOINT = f"{BACKEND_API}{settings.API_V1_STR}/auth/register"
 LOGIN_ENDPOINT = f"{BACKEND_API}{settings.API_V1_STR}/auth/login"
-CURRENT_USER = f"{BACKEND_API}{settings.API_V1_STR}/users/me"
+CURRENT_USER_ENDPOINT = f"{BACKEND_API}{settings.API_V1_STR}/users/me"
 
 
 def register_user_via_api(username: str, email: str, password: str) -> Response:
@@ -27,5 +28,7 @@ def login_via_api(email: str, password: str) -> Response:
     return response
 
 
-def get_current_user_data() -> Response:
-    pass
+def get_current_user_data_via_api(access_token: str) -> Response:
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response: Response = requests.get(CURRENT_USER_ENDPOINT, headers=headers)
+    return response
